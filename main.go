517e498367225a9e1ccf0757cf4c855fe16144b8
TimLai666 <tim930102@icloud.com>
2025-05-20 00:16:32 +0800

    新增HTTP伺服器和網頁結構，包含卡片組件及外部資源鏈接

--- main.go
+++ main.go
@@ -3,25 +3,96 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
-	"github.com/TimLai666/go-vdom/vdom"
+	. "github.com/TimLai666/go-vdom/vdom" // 使用 dot import
 )
 
 func main() {
-	Card := vdom.Component(
-		vdom.Div(
-			vdom.Props{"class": "card"},
-			vdom.H1("{{title}}"),
-			vdom.Div("{{children}}"),
+	// 定義一個卡片組件
+	Card := Component(
+		Div(
+			Props{"class": "card"},
+			H1("{{title}}"),
+			Div("{{children}}"),
 		),
 	)
 
-	html := vdom.Render(
-		Card(vdom.Props{"title": "Hello Card"},
-			vdom.Div("這是卡片的內容"),
-			vdom.Div("這是卡片的內容2"),
-		),
-	)
+	// 處理HTTP請求的函數
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// 設置內容類型為HTML
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	fmt.Println(html)
+		// 使用 Document 函數創建一個完整的 HTML 文檔
+		doc := Document(
+			"我的網頁", // 頁面標題
+			[]LinkInfo{ // 外部資源鏈接
+				{Rel: "stylesheet", Href: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css", Type: "text/css"},
+				{Rel: "icon", Href: "https://golang.org/favicon.ico", Type: "image/x-icon"},
+			},
+			[]ScriptInfo{ // JavaScript 腳本
+				{Src: "https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js", Async: true},
+			},
+			[]Props{ // Meta 標籤
+				{"name": "description", "content": "這是一個使用go-vdom創建的示例網頁"},
+				{"name": "keywords", "content": "Go, vdom, HTML"},
+			},
+			// 頁面主體內容
+			Header(
+				Props{"class": "container bg-light p-4 mb-4"},
+				H1("Go VDOM 示範網站", Props{"class": "text-primary"}),
+				Nav(
+					Props{"class": "navbar navbar-expand"},
+					Ul(
+						Props{"class": "navbar-nav"},
+						Li(Props{"class": "nav-item"}, A(Props{"href": "#", "class": "nav-link"}, "首頁")),
+						Li(Props{"class": "nav-item"}, A(Props{"href": "#about", "class": "nav-link"}, "關於我們")),
+						Li(Props{"class": "nav-item"}, A(Props{"href": "#contact", "class": "nav-link"}, "聯繫我們")),
+					),
+				),
+			),
+			Main(
+				Props{"class": "container"},
+				H2("歡迎訪問", Props{"class": "mt-4"}),
+				P("這是一個使用 go-vdom 創建的網頁示例。該頁面直接從Go HTTP伺服器產生。"),
+				Card(Props{"title": "使用組件"},
+					Div(Props{"class": "mb-3"}, "這是卡片的內容，展示了組件的使用方法"),
+					Div(Props{"class": "text-muted"}, "可以在組件中傳入多個子元素"),
+				),
+				Div(
+					Props{"class": "row mt-4"},
+					Div(
+						Props{"class": "col-md-6"},
+						H3("左側內容"),
+						P("這是左側的一些內容，展示了多欄排版的效果。"),
+						Ul(
+							Li("項目1"),
+							Li("項目2"),
+							Li("項目3"),
+						),
+					),
+					Div(
+						Props{"class": "col-md-6"},
+						H3("右側內容"),
+						P("這是右側的一些內容。"),
+						A(Props{"href": "https://github.com/TimLai666/go-vdom", "class": "btn btn-primary"}, "查看源碼"),
+					),
+				),
+			),
+			Footer(
+				Props{"class": "container bg-light p-4 mt-4"},
+				P(Props{"class": "text-center"}, "© 2025 Go VDOM 示範網站 | 使用 Go 和 VDOM 製作"),
+			),
+		)
+
+		// 渲染 HTML 並寫入 HTTP 回應
+		html := Render(doc)
+		fmt.Fprint(w, html)
+	})
+
+	// 啟動 HTTP 伺服器
+	port := ":8080"
+	fmt.Printf("伺服器已啟動，請訪問 http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
--- vdom/tags.go
+++ vdom/tags.go
@@ -35,7 +35,72 @@ func tag(name string, args ...any) VNode {
 	}
 }
 
-// HTML 標籤函數
+// LinkInfo 定義外部資源鏈接的信息
+type LinkInfo struct {
+	Rel  string // 關聯類型，如 "stylesheet", "icon", "preload" 等
+	Href string // 鏈接地址
+	Type string // 媒體類型，如 "text/css"
+}
+
+// ScriptInfo 定義JavaScript腳本的信息
+type ScriptInfo struct {
+	Src   string // 腳本地址
+	Async bool   // 是否異步加載
+}
+
+// Document 創建一個完整的HTML文檔結構
+// 參數:
+// - title: 頁面標題
+// - links: 要加入的外部資源鏈接列表
+// - scripts: 要加入的JavaScript腳本列表
+// - metas: 要加入的meta標籤屬性列表
+// - bodyContent: 頁面主體內容
+func Document(title string, links []LinkInfo, scripts []ScriptInfo, metas []Props, bodyContent ...VNode) VNode {
+	// 頭部元素列表
+	var headElements []any
+
+	// 添加標題
+	headElements = append(headElements, Title(title))
+
+	// 添加基本meta標籤
+	headElements = append(headElements, Meta(Props{"charset": "UTF-8"}))
+	headElements = append(headElements, Meta(Props{"name": "viewport", "content": "width=device-width, initial-scale=1.0"}))
+
+	// 添加自定義meta標籤
+	for _, meta := range metas {
+		headElements = append(headElements, Meta(meta))
+	}
+
+	// 添加外部資源鏈接
+	for _, link := range links {
+		props := Props{"rel": link.Rel, "href": link.Href}
+		if link.Type != "" {
+			props["type"] = link.Type
+		}
+		headElements = append(headElements, Link(props))
+	}
+
+	// 添加腳本
+	for _, script := range scripts {
+		props := Props{"src": script.Src}
+		if script.Async {
+			props["async"] = "true"
+		}
+		headElements = append(headElements, Script(props))
+	}
+
+	// 將body內容轉換為any類型
+	var bodyElements []any
+	for _, node := range bodyContent {
+		bodyElements = append(bodyElements, node)
+	}
+
+	return Html(
+		Head(headElements...),
+		Body(bodyElements...),
+	)
+}
+
 // HTML 結構元素
 func Html(args ...any) VNode    { return tag("html", args...) }
 func Head(args ...any) VNode    { return tag("head", args...) }
@@ -47,6 +112,8 @@ func Nav(args ...any) VNode     { return tag("nav", args...) }
 func Aside(args ...any) VNode   { return tag("aside", args...) }
 func Section(args ...any) VNode { return tag("section", args...) }
 func Article(args ...any) VNode { return tag("article", args...) }
+func Address(args ...any) VNode { return tag("address", args...) }
+func Hgroup(args ...any) VNode  { return tag("hgroup", args...) }
 
 // 文本區塊元素
 func H1(args ...any) VNode         { return tag("h1", args...) }
@@ -63,15 +130,20 @@ func Code(args ...any) VNode       { return tag("code", args...) }
 func Blockquote(args ...any) VNode { return tag("blockquote", args...) }
 
 // 表單元素
-func Form(args ...any) VNode     { return tag("form", args...) }
-func Input(args ...any) VNode    { return tag("input", args...) }
-func Label(args ...any) VNode    { return tag("label", args...) }
-func Button(args ...any) VNode   { return tag("button", args...) }
-func Select(args ...any) VNode   { return tag("select", args...) }
-func Option(args ...any) VNode   { return tag("option", args...) }
-func Textarea(args ...any) VNode { return tag("textarea", args...) }
-func Fieldset(args ...any) VNode { return tag("fieldset", args...) }
-func Legend(args ...any) VNode   { return tag("legend", args...) }
+func Form(args ...any) VNode       { return tag("form", args...) }
+func Input(args ...any) VNode      { return tag("input", args...) }
+func Label(args ...any) VNode      { return tag("label", args...) }
+func Button(args ...any) VNode     { return tag("button", args...) }
+func Select(args ...any) VNode     { return tag("select", args...) }
+func Datalist(args ...any) VNode   { return tag("datalist", args...) }
+func Optgroup(args ...any) VNode   { return tag("optgroup", args...) }
+func Option(args ...any) VNode     { return tag("option", args...) }
+func Textarea(args ...any) VNode   { return tag("textarea", args...) }
+func Output(args ...any) VNode     { return tag("output", args...) }
+func Progress(args ...any) VNode   { return tag("progress", args...) }
+func Meter(args ...any) VNode      { return tag("meter", args...) }
+func Fieldset(args ...any) VNode   { return tag("fieldset", args...) }
+func Legend(args ...any) VNode     { return tag("legend", args...) }
 
 // 表格元素
 func Table(args ...any) VNode   { return tag("table", args...) }
@@ -82,6 +154,8 @@ func Tr(args ...any) VNode      { return tag("tr", args...) }
 func Th(args ...any) VNode      { return tag("th", args...) }
 func Td(args ...any) VNode      { return tag("td", args...) }
 func Caption(args ...any) VNode { return tag("caption", args...) }
+func Colgroup(args ...any) VNode { return tag("colgroup", args...) }
+func Col(args ...any) VNode     { return tag("col", args...) }
 
 // 列表元素
 func Ul(args ...any) VNode { return tag("ul", args...) }
@@ -92,33 +166,71 @@ func Dt(args ...any) VNode { return tag("dt", args...) }
 func Dd(args ...any) VNode { return tag("dd", args...) }
 
 // 媒體元素
-func Img(args ...any) VNode    { return tag("img", args...) }
-func Audio(args ...any) VNode  { return tag("audio", args...) }
-func Video(args ...any) VNode  { return tag("video", args...) }
-func Source(args ...any) VNode { return tag("source", args...) }
-func Canvas(args ...any) VNode { return tag("canvas", args...) }
-func Svg(args ...any) VNode    { return tag("svg", args...) }
+func Img(args ...any) VNode      { return tag("img", args...) }
+func Audio(args ...any) VNode    { return tag("audio", args...) }
+func Video(args ...any) VNode    { return tag("video", args...) }
+func Source(args ...any) VNode   { return tag("source", args...) }
+func Track(args ...any) VNode    { return tag("track", args...) }
+func Map(args ...any) VNode      { return tag("map", args...) }
+func Area(args ...any) VNode     { return tag("area", args...) }
+func Canvas(args ...any) VNode   { return tag("canvas", args...) }
+func Figure(args ...any) VNode   { return tag("figure", args...) }
+func Figcaption(args ...any) VNode { return tag("figcaption", args...) }
+func Picture(args ...any) VNode  { return tag("picture", args...) }
+func Svg(args ...any) VNode      { return tag("svg", args...) }
 
 // 互動元素
-func A(args ...any) VNode       { return tag("a", args...) }
-func Details(args ...any) VNode { return tag("details", args...) }
-func Summary(args ...any) VNode { return tag("summary", args...) }
-func Dialog(args ...any) VNode  { return tag("dialog", args...) }
-func Menu(args ...any) VNode    { return tag("menu", args...) }
+func A(args ...any) VNode         { return tag("a", args...) }
+func Details(args ...any) VNode   { return tag("details", args...) }
+func Summary(args ...any) VNode   { return tag("summary", args...) }
+func Dialog(args ...any) VNode    { return tag("dialog", args...) }
+func Menu(args ...any) VNode      { return tag("menu", args...) }
 
 // 腳本元素
-func Script(args ...any) VNode { return tag("script", args...) }
-func Style(args ...any) VNode  { return tag("style", args...) }
-func Link(args ...any) VNode   { return tag("link", args...) }
-func Meta(args ...any) VNode   { return tag("meta", args...) }
-func Title(args ...any) VNode  { return tag("title", args...) }
-
-// 其他常用元素
-func Br(args ...any) VNode       { return tag("br", args...) }
-func Hr(args ...any) VNode       { return tag("hr", args...) }
-func Iframe(args ...any) VNode   { return tag("iframe", args...) }
+func Script(args ...any) VNode   { return tag("script", args...) }
 func Noscript(args ...any) VNode { return tag("noscript", args...) }
-func Time(args ...any) VNode     { return tag("time", args...) }
-func Abbr(args ...any) VNode     { return tag("abbr", args...) }
-func Strong(args ...any) VNode   { return tag("strong", args...) }
-func Em(args ...any) VNode       { return tag("em", args...) }
+func Template(args ...any) VNode { return tag("template", args...) }
+func Slot(args ...any) VNode     { return tag("slot", args...) }
+func Style(args ...any) VNode    { return tag("style", args...) }
+func Link(args ...any) VNode     { return tag("link", args...) }
+func Meta(args ...any) VNode     { return tag("meta", args...) }
+func Title(args ...any) VNode    { return tag("title", args...) }
+func Base(args ...any) VNode     { return tag("base", args...) }
+
+// 內聯文本元素
+func Abbr(args ...any) VNode        { return tag("abbr", args...) }
+func B(args ...any) VNode           { return tag("b", args...) }
+func Bdi(args ...any) VNode         { return tag("bdi", args...) }
+func Bdo(args ...any) VNode         { return tag("bdo", args...) }
+func Br(args ...any) VNode          { return tag("br", args...) }
+func Cite(args ...any) VNode        { return tag("cite", args...) }
+func Data(args ...any) VNode        { return tag("data", args...) }
+func Dfn(args ...any) VNode         { return tag("dfn", args...) }
+func Em(args ...any) VNode          { return tag("em", args...) }
+func I(args ...any) VNode           { return tag("i", args...) }
+func Kbd(args ...any) VNode         { return tag("kbd", args...) }
+func Mark(args ...any) VNode        { return tag("mark", args...) }
+func Q(args ...any) VNode           { return tag("q", args...) }
+func Rb(args ...any) VNode          { return tag("rb", args...) }
+func Rp(args ...any) VNode          { return tag("rp", args...) }
+func Rt(args ...any) VNode          { return tag("rt", args...) }
+func Rtc(args ...any) VNode         { return tag("rtc", args...) }
+func Ruby(args ...any) VNode        { return tag("ruby", args...) }
+func S(args ...any) VNode           { return tag("s", args...) }
+func Samp(args ...any) VNode        { return tag("samp", args...) }
+func Small(args ...any) VNode       { return tag("small", args...) }
+func Strong(args ...any) VNode      { return tag("strong", args...) }
+func Sub(args ...any) VNode         { return tag("sub", args...) }
+func Sup(args ...any) VNode         { return tag("sup", args...) }
+func Time(args ...any) VNode        { return tag("time", args...) }
+func U(args ...any) VNode           { return tag("u", args...) }
+func Var(args ...any) VNode         { return tag("var", args...) }
+func Wbr(args ...any) VNode         { return tag("wbr", args...) }
+
+// 其他元素
+func Hr(args ...any) VNode          { return tag("hr", args...) }
+func Iframe(args ...any) VNode      { return tag("iframe", args...) }
+func Object(args ...any) VNode      { return tag("object", args...) }
+func Param(args ...any) VNode       { return tag("param", args...) }
+func Embed(args ...any) VNode       { return tag("embed", args...) }
+func Math(args ...any) VNode        { return tag("math", args...) }
